520c4cc89fa99568f116e122b13b67adecfcfda7
jamesvrooney <jamesvrooney@hotmail.com>
2022-12-27 19:32:19 +0000

    Initial commit.

---
+++ src/main/java/com/jamesvrooney/SpringSecurityFundamentals2020Application.java
@@ -0,0 +1,13 @@
+package com.jamesvrooney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFundamentals2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityFundamentals2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamesvrooney/SpringSecurityFundamentals2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesvrooney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFundamentals2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
