59e3add1bfcd280c5776c84e6d0af95509d5b100
jamesvrooney <jamesvrooney@hotmail.com>
2022-12-29 07:04:18 +0000

    Added more logging.

--- src/main/java/com/jamesvrooney/services/JPAUserDetailsService.java
+++ src/main/java/com/jamesvrooney/services/JPAUserDetailsService.java
@@ -3,11 +3,13 @@ package com.jamesvrooney.services;
 import com.jamesvrooney.entities.SecurityUser;
 import com.jamesvrooney.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @AllArgsConstructor
 @Service
 public class JPAUserDetailsService implements UserDetailsService {
@@ -21,6 +23,8 @@ public class JPAUserDetailsService implements UserDetailsService {
         final SecurityUser userFromDb = user.map(SecurityUser::new)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
+        log.info("User from database retrieved: {}", userFromDb.getUsername());
+        
         return userFromDb;
     }
 }
