c8840d2de1d8e608fb2b0b2ac9c992290c1344e2
jamesvrooney <jamesvrooney@hotmail.com>
2022-12-30 07:20:33 +0000

    Implemented CustomAuthenticationProvider.

--- src/main/java/com/jamesvrooney/controllers/DemoController.java
+++ src/main/java/com/jamesvrooney/controllers/DemoController.java
@@ -2,6 +2,7 @@ package com.jamesvrooney.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     @GetMapping("demo")
-    public String demo() {
+    public String demo(Authentication authentication) {
 
         log.info("Testing /demo endpoint");
 
-        final var greeting = "Hello demo";
+        var name = authentication.getName();
+
+        final var greeting = "Hello " + name;
 
         return greeting;
     }
